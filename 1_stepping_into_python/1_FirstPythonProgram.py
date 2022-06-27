c3fb5238e84c5b9f5f7ebfd672fbfb0daf95d63f
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-06-27 14:45:49 -0500

    Printing in python

---
+++ 1_stepping_into_python/1_FirstPythonProgram.py
@@ -0,0 +1,10 @@
+"""
+Python has many functions we will be using as we
+learn more about Python. We can even create our own
+functions
+
+functions used:
+print()
+"""
+print('Hello, world!')
+
---
+++ 1_stepping_into_python/2_Printing.py
@@ -0,0 +1,15 @@
+"""
+Printing in Python
+Can use either '' or ""
+The 'Hello, world!' is called an argument that
+is being passed to the print function
+Can also print calculations and a blank line
+Can also pass multiple arguments
+"""
+print('Hello, world!')
+print(1 + 2)
+print(7 * 6)
+print()
+print("The end", "or is it?", "Keep watching to learn more about Python", 3)
+
+
