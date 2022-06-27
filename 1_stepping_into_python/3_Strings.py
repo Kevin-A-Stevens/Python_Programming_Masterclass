6559bad294aec5544daa542fcb5c570119f523ef
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-06-27 14:56:58 -0500

    Strings in Python

---
+++ 1_stepping_into_python/3_Strings.py
@@ -0,0 +1,25 @@
+"""
+Strings in python
+Can use either "" or ''
+Adding strings together is called string concatenation
+
+Functions used:
+print()
+input()
+"""
+print("Today is a good day to learn Python")
+print('Python is fun')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("Hello" + " world")
+
+# Can also store strings in a variable to be used later
+greeting = "Hello"
+name = "Kevin"
+
+# if we want a space, we can add that also
+print(greeting + ' ' + name)
+
+# The input function
+iname = input("Please enter your name: ")
+print(greeting + ' ' + iname)
