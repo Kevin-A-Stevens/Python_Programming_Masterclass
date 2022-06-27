a8188da72ba8421532c7b15bb69edbcc2320eed5
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-06-27 15:16:37 -0500

    Escape characters in Python

---
+++ 1_stepping_into_python/4_EscapeChar.py
@@ -0,0 +1,34 @@
+"""
+Escape characters
+
+Functions used:
+print()
+"""
+
+# new line
+splitString = "This strings has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+# tab
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+# escape character
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+
+# Using """
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting". """)
+
+anotherSplitString = """This string has been
+split over
+several
+lines"""
+print(anotherSplitString)
+
+# The \ cna escape an end of the line
+anotherWaySplitString = """This string has been \
+split over \
+several \
+lines"""
+print(anotherWaySplitString)
