1ac9d10aa65cb51430e856f8fef5545acb8311e2
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-06-27 16:13:45 -0500

    String indexing in Python

---
+++ 1_stepping_into_python/10_StringDataType.py
@@ -0,0 +1,31 @@
+"""
+String Data Type and indexing
+
+Functions used:
+print()
+"""
+
+# Using index position. Indexes start at 0
+parrot = "Norwegian Blue"
+print(parrot)
+print(parrot[3])
+print(parrot[4])
+print(parrot[9])
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+print()
+
+# Negative indexing
+print(parrot[-1])
+print(parrot[-14])
+
+print()
+
+print(parrot[-11])
+print(parrot[-10])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
