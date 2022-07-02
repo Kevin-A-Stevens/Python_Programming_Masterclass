769e9601c3662b4446f7d772cd740cfb69d4d020
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-07-01 22:28:12 -0500

    String formatting in Python

---
+++ 1_stepping_into_python/12_SequenceOperators.py
@@ -0,0 +1,20 @@
+string1="he's "
+string2="probably "
+string3 = "pining "
+string4 = "for the "
+string5 = "fjords"
+
+# Can add strings together
+print(string1 + string2 + string3 + string4 + string5)
+print("he's " "probably " "pining " "for the " "fjords")
+
+# Can multiply strings
+print("Hello " * 5)
+print("Hello " * (5 + 4))
+print("Hello " * 5 + "4")
+
+# Check if string contains a substring
+today = "Friday"
+print("day" in today)
+print("Fri" in today)
+print("thu" in today)
---
+++ 1_stepping_into_python/14_StringFormatting.py
@@ -0,0 +1,26 @@
+"""
+String formatting
+Specify a width {#:width}
+Left align {#:<width}
+Right align {#:>width}
+Center {#:^width}
+Floating point {#:widthf} default is 6
+Floating point {#:width.50f} 50 places after decimal point
+"""
+for i in range(1, 13):
+    print("No. {0:2} squared is {1:3} and cubed is {2:4}".format(i,  i**2, i**3))
+
+print()
+
+for i in range(1, 13):
+    print("No. {0:2} squared is {1:<3} and cubed is {2:^4}".format(i,  i**2, i**3))
+
+print()
+
+print("Pi is approximately {0:12}".format(22 / 7))
+print("Pi is approximately {0:12f}".format(22 / 7))
+print("Pi is approximately {0:12.50f}".format(22 / 7))
+print("Pi is approximately {0:52.50f}".format(22 / 7))
+print("Pi is approximately {0:62.50f}".format(22 / 7))
+print("Pi is approximately {0:<72.50f}".format(22 / 7))
+print("Pi is approximately {0:<72.54f}".format(22 / 7))
