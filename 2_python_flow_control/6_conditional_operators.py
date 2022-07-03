3a7f8ec93443f2a55444e6b193bf06c063f01c29
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-07-02 22:04:23 -0500

    conditional operators in Python

---
+++ 2_python_flow_control/6_conditional_operators.py
@@ -0,0 +1,28 @@
+"""
+Conditional Operators
+Less than <
+Less than or equal to <=
+Greater than >
+Greater than or equal to >=
+equal to ==
+Not equal to !=
+
+We can now improve our guessing game
+"""
+answer = 5
+
+print("Please guess a number between 1 and 10: ")
+guess = int(input())
+
+if guess != answer:
+    if guess < answer:
+        print("Please guess higher")
+    else:
+        print("Please guess lower")
+    guess = int(input())
+    if guess == answer:
+        print("Well done, you guessed it")
+    else:
+        print("Sorry, you have not guessed correctly")
+else:
+    print("You got it first time")
