fa4235c055a532c762be002358153998cfcbb129
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-07-03 15:26:40 -0500

    while loops in Python

---
+++ 2_python_flow_control/15_while_loops.py
@@ -0,0 +1,15 @@
+"""
+while loops are used to keep looping as long as a condition
+remains True
+Use while loops if you do not know how long the loop will run
+
+while <condition>:
+    code block
+"""
+
+i = 0
+while i < 10:
+    print("i is now {}".format(i))
+    i += 1
+
+
