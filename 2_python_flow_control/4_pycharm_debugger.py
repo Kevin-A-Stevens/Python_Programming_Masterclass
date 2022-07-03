1d4d79b639464e8d6608a3f39414ca18720ca014
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-07-02 21:33:11 -0500

    pycharm debugger in Python

---
+++ 2_python_flow_control/4_pycharm_debugger.py
@@ -0,0 +1,14 @@
+"""
+Pycharm debugger
+right-click and choose 'Debug program_name'
+Will have 2 tabs (Debugger and Console)
+click in the left margin where to set a break point
+click the break point again to turn it off
+right click and choose Debug option again
+Enter information and program will stop at break point
+You are now in debugger tab
+Click the Evaluate expression far right of the buttons
+Here can type in different expressions in your program
+The step over button skips the line
+Continue to click on step over through until the end
+"""
