de1ba5ba039ed3a9ec0dfe9a7a66af5454afdc9d
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-07-03 18:52:54 -0500

    augmented assignments in Python

---
+++ 2_python_flow_control/19_augmented_assignments.py
@@ -0,0 +1,26 @@
+"""
+augmented assignments
+"""
+x = 23
+print(x)
+x += 1
+print(x)
+x -= 4
+print(x)
+x *= 5
+print(x)
+x //= 4
+print(x)
+x /= 5
+print(x)
+x **= 2
+print(x)
+x %= 5
+print(x)
+
+greeting = "Good "
+greeting += "morning"
+print(greeting)
+
+greeting *= 5
+print(greeting)
