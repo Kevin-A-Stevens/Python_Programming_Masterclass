e1c1debb28129a64db0e2df0d481d27c37228983
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-07-03 23:05:45 -0500

    case insensitive sorting in Python

--- 3_lists_and_tuples/8_sorting_list.py
+++ 3_lists_and_tuples/8_sorting_list.py
@@ -1,9 +1,13 @@
 """
-sorting a list
+sorting a list = two ways to sort
+
+functions used
+print()
+sorted() = sort any iterable object. returns a different list
 
 methods used
 .extend() = adds a list to a list
-.sort() = sorts a list
+.sort() = sorts a list in place and returns None
 """
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
@@ -20,3 +24,33 @@ even.sort(reverse=True)
 print(even)
 print(another_even)
 
+pangram = "The quick brown fox jumps over the lazy dog"
+letters = sorted(pangram)
+print(letters)
+
+numbers = [2.3, 4.5, 8.7, 3.1, 9.2, 1.6]
+sorted_numbers = sorted(numbers)
+print(sorted_numbers)
+print(numbers)
+
+numbers.sort()  # in place sort
+print(numbers)
+
+another_sorted_numbers = numbers.sort()
+print(numbers)
+print(another_sorted_numbers)
+
+# case insensitive sorting
+missing_letter = sorted("The quick brown fox jumped over the lazy dog",
+                        key=str.casefold)
+print(missing_letter)
+
+names = ["Graham",
+        "John",
+        "terry",
+        "eric",
+        "Terry",
+        "michael",
+        ]
+names.sort(key=str.casefold)
+print(names)
