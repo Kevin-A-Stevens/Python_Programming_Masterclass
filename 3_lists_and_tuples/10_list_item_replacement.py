38a220cdb7537b6752a24487be4ad9b228b40900
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-07-04 19:44:43 -0500

    replace list items in Python

---
+++ 3_lists_and_tuples/10_list_item_replacement.py
@@ -0,0 +1,25 @@
+computer_parts = ["computer",
+                  "monitor",
+                  "keyboard",
+                  "mouse",
+                  "mouse pad",
+                  ]
+print(computer_parts)
+
+# replace the item "mouse" with the item "trackball"
+computer_parts[3] = "trackball"
+print(computer_parts)
+
+computer_parts = ["computer",
+                  "monitor",
+                  "keyboard",
+                  "mouse",
+                  "mouse pad",
+                  ]
+
+print(computer_parts[3:])
+
+# replace mouse and mouse pad with trackball
+computer_parts[3:] = ["trackball"]
+print(computer_parts)
+
