43b4659b624ee678371fc3fade9563de1f83958f
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-07-07 21:23:04 -0500

    print function in Python

---
+++ 3_lists_and_tuples/13_print_function.py
@@ -0,0 +1,18 @@
+"""
+The print function
+can pass multiple arguments
+many objects
+sep kw argument = seperator with space the default
+end kw argument = end of line with new line the default
+functions used
+print()
+
+"""
+name = "Kevin"
+age = 55
+
+print(name, age, "Python", 2022)
+print(name, age, "Python", 2022, sep=", ")
+print(name, age, "Python", 2022, sep=", ", end=" ")
+print(name, age, "Python", 2022)
+
