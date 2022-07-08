03f8e0533c0720e1c34ce6d5cfeb77cecde0a4be
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-07-07 21:51:47 -0500

    split method in Python

---
+++ 3_lists_and_tuples/15_split_method.py
@@ -0,0 +1,18 @@
+"""
+the split method
+
+functions used
+print()
+
+methods used
+.split() = opposite of join. default is split on spaces
+split returns a list
+"""
+
+panagram = "The quick brown fox jumps over the lazy dog"
+
+words = panagram.split()
+print(words)
+
+numbers = "9,223,373,036,854,775,807"
+print(numbers.split(","))
