8b1614f18babae806f9456fd7576aed3265461f7
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-07-10 19:49:26 -0500

    palindromes in Python

--- 4_functions/2_palindromes.py
+++ 4_functions/2_palindromes.py
@@ -2,6 +2,13 @@
 A palindrome is a word that reads the same
 backwards and forwards
 
+functions used
+print()
+input()
+
+methods used
+.isalnum()
+.casefold()
 """
 
 
@@ -10,12 +17,25 @@ def is_palindrome(string):
     # returns True/False
 
 
+def palindrome_sentence(sentence):
+    string = ""
+    for char in sentence:
+        if char.isalnum():
+            string += char
+
+    return is_palindrome(string)
+
+
 word = input("Please enter a word to check: ")
 if is_palindrome(word):
     print("'{}' is a palindrome".format(word))
 else:
     print("'{}' is not a palindrome".format(word))
 
+if palindrome_sentence(word):
+    print("'{}' is a palindrome".format(word))
+else:
+    print("'{}' is not a palindrome".format(word))
 
 
 
