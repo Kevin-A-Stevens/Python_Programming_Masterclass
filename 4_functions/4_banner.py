fe81c900f10e71bb3927c990ffb64db5bb126a9a
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-07-15 21:17:41 -0500

    raising exceptions in Python

--- 4_functions/4_banner.py
+++ 4_functions/4_banner.py
@@ -4,6 +4,9 @@ not all functions return something useful
 sometimes you want to write a function that
 performs an action but does not return anything
 
+handling invalid arguments with raise ValueError
+(raise ExceptionType)
+
 functions used
 print()
 len()
@@ -14,11 +17,10 @@ methods used
 """
 
 
-def banner_text(text):
-    screen_width = 80
+def banner_text(text, screen_width):
     if len(text) > screen_width - 4:
-        print("EEK!!")
-        print("The text is too long to fit in the specified width")
+        raise ValueError("String {0} is larger than specified width {1}"
+                         .format(text, screen_width))
 
     if text == "*":
         print("*" * screen_width)
@@ -28,14 +30,14 @@ def banner_text(text):
         print(output_string)
 
 
-banner_text("*")
-banner_text("Always look on the bright side of life...")
-banner_text("If life seems jolly rotten,")
-banner_text("There's something you've forgotten!")
-banner_text("And that's to laugh and smile and dance and sing,")
-banner_text(" ")
-banner_text("When you're feeling in the dumps,")
-banner_text("Don't be silly chumps,")
-banner_text("Just purse your lips and whistle - that's the thing!")
-banner_text("And... always look on the bright side of life...")
-banner_text("*")
+banner_text("*", 66)
+banner_text("Always look on the bright side of life...", 66)
+banner_text("If life seems jolly rotten,", 66)
+banner_text("There's something you've forgotten!", 66)
+banner_text("And that's to laugh and smile and dance and sing,", 66)
+banner_text(" ", 66)
+banner_text("When you're feeling in the dumps,", 66)
+banner_text("Don't be silly chumps,", 66)
+banner_text("Just purse your lips and whistle - that's the thing!", 66)
+banner_text("And... always look on the bright side of life...", 66)
+banner_text("*", 66)
