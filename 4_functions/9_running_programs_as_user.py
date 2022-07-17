582adbd98654b206c6a5fad254819af621843238
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-07-16 23:46:50 -0500

    color printing in Python

---
+++ 4_functions/9_running_programs_as_user.py
@@ -0,0 +1,14 @@
+"""
+In Pycharm
+Right click on python program in left pane
+Open in > Terminal
+pwd to get full directory name of program file
+copy full path
+Open a command terminal
+cd <paste full path>
+python3 file_name.py
+
+It is useful to test your programs on the three main
+systems (Linux, Mac, Windows)
+"""
+
