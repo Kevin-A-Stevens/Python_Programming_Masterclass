520e2374c76a23f9ea343fd47e6d552cffd5541a
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-07-17 09:17:17 -0500

    testing functions in Python

--- 4_functions/9_running_programs_as_user.py
+++ 4_functions/9_running_programs_as_user.py
@@ -10,5 +10,17 @@ python3 file_name.py
 
 It is useful to test your programs on the three main
 systems (Linux, Mac, Windows)
+
+When running programs in your IDE such as Pycharm
+the IDE creates a virtual environment for us
+Note the first line
+/home/kevinstevens/venv/Python3.8/bin/python
+
+When running as a user, you may need to activate
+the virtual environment
+source /home/kevinstevens/venv/Python3.8/bin/activate
+to deactivate when finished
+deactivate
 """
+
 
