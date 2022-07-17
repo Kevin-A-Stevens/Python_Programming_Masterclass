aee4cbdc7595979ff528c2103951630c50bbcf6d
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-07-17 14:11:23 -0500

    using 'in' in dictionaries in Python

--- 5_dictionaries_and_sets/5_removing_items.py
+++ 5_dictionaries_and_sets/5_removing_items.py
@@ -1,5 +1,14 @@
 """
-changing values in a dictionary
+removing items from a dictionary
+
+can use del to remove items but the program
+will crash if the key does not exist
+
+methods used
+.pop() = can use a default value
+the default value of None will return None
+if the key does not exist instead of crashing
+
 """
 vehicles = {
     'dream': 'Honda 250T',
@@ -19,6 +28,19 @@ vehicles["toy"] = "glider"
 
 # changing a value in a dictionary
 vehicles["virago"] = "Yamaha XV535"
+
+# removing a value from a dictionary
+del vehicles["starfighter"]
+result = vehicles.pop("f1", "Does not exist")
+print(result)
+plane = vehicles.pop("learjet")
+print(plane)
+
+bike = vehicles.pop("tenere", "not found")
+print(bike)
+
+
+print()
 
 # more efficient to use the .items() method
 for key, value in vehicles.items():
---
+++ 5_dictionaries_and_sets/6_zen.py
@@ -0,0 +1,4 @@
+"""
+printing out the zen of Python
+"""
+import this
