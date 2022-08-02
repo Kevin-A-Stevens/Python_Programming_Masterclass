987a7a5cd723f83b1ac6fa7fe88f3e6196210e06
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-08-01 19:28:34 -0500

    webbrowser module

---
+++ 7_modules_and_functions/1_turtle_demo.py
@@ -0,0 +1,24 @@
+"""
+Modules and import
+
+turtle demo
+
+Methods used
+.done()
+.forward()
+.right()
+.circle()
+
+"""
+from turtle import forward, right, done
+import turtle
+
+# No need to put turtle.forward or turtle.right as we imported
+# these above
+forward(150)
+right(250)
+turtle.circle(75)
+forward(150)
+
+done()
+
---
+++ 7_modules_and_functions/3_webbrowser_module.py
@@ -0,0 +1,9 @@
+"""
+webbrowser module = opens a web page in your web browser
+"""
+import webbrowser
+
+webbrowser.open('https://www.python.org')
+
+help(webbrowser)
+
