bb254d9fe1b4271d006c92e8c7769203fa8ce4f1
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-08-13 20:45:19 -0500

    blackjack complete game in Python

---
+++ 7_modules_and_functions/20_importing.py
@@ -0,0 +1,16 @@
+"""
+Importing techniques
+
+add
+if __name__ == "__main__":
+to your code you want to import
+as shown in blackjack.py
+
+This stops you code from being executed
+when you import the module
+"""
+import blackjack
+
+print(__name__)
+blackjack.play()
+
