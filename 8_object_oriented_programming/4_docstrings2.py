2f28c3c70901846bb7a5a094f49072551d51ae3d
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-08-19 19:54:18 -0500

    programming using OOP in Python

--- 8_object_oriented_programming/4_docstrings2.py
+++ 8_object_oriented_programming/4_docstrings2.py
@@ -32,7 +32,7 @@ class Song:
             artist (Artist): An Artist object representing the creator
             duration (Optional(int)): value for the `duration` attribute
         """
-        self.title = title
+        self.name = title
         self.artist = artist
         self.duration = duration
 
@@ -64,14 +64,17 @@ class Album:
         """ Adds a song to the track list
 
         Args:
-            song (Song): a song to add
+            song (Song): The title of a song to add
             position (Optional(int): The song will be added to that position
 
         """
+        song_found = find_object(song, self.tracks)
+        if song_found is None:
+            song_found = Song(song, self.artist)
         if position is None:
-            self.tracks.append(song)
+            self.tracks.append(song_found)
         else:
-            self.tracks.insert(position, song)
+            self.tracks.insert(position, song_found)
 
 
 class Artist:
@@ -97,6 +100,25 @@ class Artist:
         """
         self.albums.append(album)
 
+    def add_song(self, name, year, title):
+        """ Add a new song to the collection of albums
+
+        This method will add the song to an album in the collection
+
+        Args:
+            name (str): The name of the album
+            year (int): The year the album was produced
+            title (str): The title of the song
+        """
+        album_found = find_object(name, self.albums)
+        if album_found is None:
+            print(name + " not found")
+            album_found = Album(name, year, self)
+            self.add_album(album_found)
+        else:
+            print("Found album " + name)
+        album_found.add_song(title)
+
 
 def find_object(field, object_list):
     """Check `object_list` to see if an object with a `name`
@@ -109,8 +131,7 @@ def find_object(field, object_list):
 
 
 def load_data():
-    new_artist = None
-    new_album = None
+
     artist_list = []
 
     with open("albums.txt", "r") as albums:
@@ -119,28 +140,12 @@ def load_data():
             year_field = int(year_field)
             print("{}:{}:{}:{}".format(artist_field, album_field, year_field, song_field))
 
+            new_artist = find_object(artist_field, artist_list)
             if new_artist is None:
                 new_artist = Artist(artist_field)
                 artist_list.append(new_artist)
-            elif new_artist.name != artist_field:
-                new_artist = find_object(artist_field, artist_list)
-                if new_artist is None:
-                    new_artist = Artist(artist_field)
-                    artist_list.append(new_artist)
-                new_album = None
 
-            if new_album is None:
-                new_album = Album(album_field, year_field, new_artist)
-                new_artist.add_album(new_album)
-            elif new_album.name != album_field:
-                new_album = find_object(album_field, new_artist.albums)
-                if new_album is None:
-                    new_album = Album(album_field, year_field, new_artist)
-                    new_artist.add_album(new_album)
-
-            # Create a new song object and add it to current album
-            new_song = Song(song_field, new_artist)
-            new_album.add_song(new_song)
+            new_artist.add_song(album_field, year_field, song_field)
 
     return artist_list
 
@@ -153,7 +158,7 @@ def create_checkfile(artist_list):
         for new_artist in artist_list:
             for new_album in new_artist.albums:
                 for new_song in new_album.tracks:
-                    print("{0.name}\t{1.name}\t{1.year}\t{2.title}".format(new_artist, new_album, new_song),
+                    print("{0.name}\t{1.name}\t{1.year}\t{2.name}".format(new_artist, new_album, new_song),
                           file=checkfile)
 
 
