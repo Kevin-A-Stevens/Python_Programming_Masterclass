63dc845544a46eae6c87fd46c84077b5ac251d2c
Kevin Stevens <kevin.alan.stevens@gmail.com>
2022-09-02 21:09:18 -0500

    timeit module in Python

---
+++ 10_advanced_python/12_nestedcomp.py
@@ -0,0 +1,21 @@
+"""
+nested comprehensions
+"""
+
+burgers = ["beef", "chicken", "spicy been"]
+toppings = ["cheese", "egg", "beans", "spam"]
+
+for meals in [(burger, topping) for burger in burgers for topping in toppings]:
+    print(meals)
+
+print()
+
+# for burger in burgers:
+#     for topping in toppings:
+#         print((burger, topping))
+
+for nested_meals in [[(burger, topping) for burger in burgers] for topping in toppings]:
+    print(nested_meals)
+
+for nested_meals in [[(burger, topping) for topping in toppings] for burger in burgers]:
+    print(nested_meals)
